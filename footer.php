fc3ea7143cf996e39b2ee5df4e9a6ac64cd3385b
César Martín Cristóbal <cesar@MacBook-Pro-de-Cesar.local>
2012-12-14 00:15:52 +0100

    inicio

---
+++ footer.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content after
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+?>
+
+	</div><!-- #main -->
+
+	<footer id="colophon" role="contentinfo">
+
+			<?php
+				/* A sidebar in the footer? Yep. You can can customize
+				 * your footer with three columns of widgets.
+				 */
+				if ( ! is_404() )
+					get_sidebar( 'footer' );
+			?>
+
+			<div id="site-generator">
+				<img src="<?php bloginfo('stylesheet_directory'); ?>/images/headers/foot.png" id="fotopie">
+				
+				<div id='license'>
+					<a href="http://creativecommons.org/licenses/by-nc-nd/3.0/deed.es_CO"><img alt="Licencia Creative Commons" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-nd/3.0/88x31.png" /></a><span style='font-size:8pt'><br />Este obra está bajo una <a href="http://creativecommons.org/licenses/by-nc-nd/3.0/deed.es_CO">Licencia Creative Commons Atribución-NoComercial-SinDerivadas 3.0 Unported</a>.</span>
+				</div>
+				<div id="info">
+						<?php do_action( 'twentyten_credits' ); ?> 
+						Sitio creado con la ayuda de <a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyten' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentyten' ); ?>" rel="generator"><?php printf( __( '%s.', 'twentyten' ), 'WordPress' ); ?></a>
+				</div>
+				<div id='errores' class='justificado'>
+					Cualquier fallo, error o sugerencia que encontréis podéis mandar un correo a <span style='font-weight:bold;'>cesarmc@fundaciondisi.com</span>, gracias!
+				</div>
+			</div>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+?><!DOCTYPE html>
+<!--[if IE 6]>
+<html id="ie6" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 7]>
+<html id="ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html id="ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<title>Fundación DISI</title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<!--[if lt IE 9]>
+<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+<![endif]-->
+<?php
+	/* We add some JavaScript to pages with the comment form
+	 * to support sites with threaded comments (when in use).
+	 */
+	if ( is_singular() && get_option( 'thread_comments' ) )
+		wp_enqueue_script( 'comment-reply' );
+
+	/* Always have wp_head() just before the closing </head>
+	 * tag of your theme, or you will break many plugins, which
+	 * generally use this hook to add elements to <head> such
+	 * as styles, scripts, and meta tags.
+	 */
+	wp_head();
+?>
+<meta name='author' content='César M. Cristóbal' />
+<link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory'); ?>/favicon.ico" />
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="hfeed">
+	<header id="branding" role="banner">
+		<nav id="access" role="navigation">
+			<div class="menu">
+				<ul>
+					<li class="current_page_item">
+						<a title="Inicio" href="<?php site_url();?>">Inicio</a>
+					</li>
+					<li class="page_item page-item-2">
+						<a href="http://localhost:8888/fundacion/sample-page/"></a>
+					</li>
+					<?php
+					global $post;
+					$args = array('category' => 2 );
+					$myposts = get_posts( $args );
+					foreach( $myposts as $post ) :	setup_postdata($post); ?>
+						<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+					<?php endforeach; ?>
+				</ul>
+			</div>
+		</nav><!-- #access -->
+			<hgroup>
+				<h1 id="site-title"><span><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span></h1>
+				<h2 id="site-description">Desarrollo-Investigación-Sostenibilidad-Innovación</h2>
+			</hgroup>
+			
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+					<img src="<?php bloginfo('stylesheet_directory'); ?>/images/headers/cab.png" id="imagencab"/>
+			</a>
+			
+
+			<?php
+				// Has the text been hidden?
+				if ( 'blank' == get_header_textcolor() ) :
+			?>
+				<div class="only-search<?php if ( $header_image ) : ?> with-image<?php endif; ?>">
+				<?php get_search_form(); ?>
+				</div>
+			<?php
+				else :
+			?>
+				<?php get_search_form(); ?>
+			<?php endif; ?>
+
+			
+	</header><!-- #branding -->
+
+
+	<div id="main">
